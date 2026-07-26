cd12fa27a941e07d799ffb96c8b6c0f9f0eef85e
Jonathan Ringer <jonringer117@gmail.com>
2026-07-26 13:07:02 -0700

    cudaPackages.tensorrt: fix broken symlinks, add bin output, allow FHS refs

--- cuda/pkgs/tensorrt.nix
+++ cuda/pkgs/tensorrt.nix
@@ -3,13 +3,21 @@ buildRedist {
   redistName = "tensorrt";
   pname = "tensorrt";
 
+  allowFHSReferences = true;
+
   outputs = [
     "out"
     "dev"
+    "bin"
     "include"
     "lib"
     "static"
   ];
+
+  # Remove broken symlinks left by multi-output splitting
+  preFixup = ''
+    find "$out" -xtype l -delete 2>/dev/null || true
+  '';
 
   meta = {
     description = "High-performance deep learning inference optimizer and runtime";
