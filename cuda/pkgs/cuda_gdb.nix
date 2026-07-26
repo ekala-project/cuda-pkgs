997e90aaf2b89af4abf35b7447119e7cc567ff0c
Jonathan Ringer <jonringer117@gmail.com>
2026-07-26 11:59:54 -0700

    cudaPackages.cuda_gdb: add gmp/expat/ncurses deps, ignore bundled python, fix outputs

--- cuda/pkgs/cuda_gdb.nix
+++ cuda/pkgs/cuda_gdb.nix
@@ -1,12 +1,29 @@
-{ buildRedist }:
+{
+  buildRedist,
+  gmp,
+  expat,
+  ncurses,
+}:
 buildRedist {
   redistName = "cuda";
   pname = "cuda_gdb";
 
+  allowFHSReferences = true;
+
   outputs = [
     "out"
     "bin"
-    "lib"
+  ];
+
+  buildInputs = [
+    gmp
+    expat
+    ncurses
+  ];
+
+  # cuda-gdb bundles a python3.8 that we can't satisfy
+  autoPatchelfIgnoreMissingDeps = [
+    "libpython*"
   ];
 
   meta = {
